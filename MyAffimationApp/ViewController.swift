89a632484c6fdce778af94423127582fdc4df9ef
Shanice Brown <s.mariebrown10@gmail.com>
2019-06-30 21:14:14 -0500

    Initial Commit

---
+++ MyAffimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAffimationApp
+//
+//  Created by User on 6/30/19.
+//  Copyright © 2019 OneSummerChicago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
